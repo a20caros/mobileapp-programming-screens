29e12be984d7c5db08ef9b9b2a495426c6020b23
a20caros <a20caros@student.his.se>
2021-04-20 10:19:56 +0200

    Skapat en fragment

--- app/src/main/java/com/example/screens/Blank_fragment.java
+++ app/src/main/java/com/example/screens/Blank_fragment.java
@@ -1,19 +1,18 @@
 package com.example.screens;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Fragment#newInstance} factory method to
+ * Use the {@link Blank_fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment extends Fragment {
+public class Blank_fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +23,7 @@ public class Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Fragment() {
+    public Blank_fragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +33,11 @@ public class Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment.
+     * @return A new instance of fragment Blank_fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment newInstance(String param1, String param2) {
-        Fragment fragment = new Fragment();
+    public static Blank_fragment newInstance(String param1, String param2) {
+        Blank_fragment fragment = new Blank_fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +58,6 @@ public class Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_, container, false);
+        return inflater.inflate(R.layout.fragment_blank_fragment, container, false);
     }
 }
